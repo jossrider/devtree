759108790c1f5ebafe91e4494469d8f7a3d90eb6
Joss Rider <jossrider@outlook.com>
2025-09-07 19:36:50 -0300

    agregamos autenticacion en el backend

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -2,15 +2,9 @@ import type { Request, Response } from 'express'
 import { validationResult } from 'express-validator'
 import slug from 'slug'
 import User from '../Models/User'
-import { hashPassword } from '../utils/auth'
+import { checkPassword, hashPassword } from '../utils/auth'
 
 export const createAccount = async (req: Request, res: Response) => {
-  // Manejar errores
-  let errors = validationResult(req)
-  if (!errors.isEmpty()) {
-    return res.status(400).json({ errors: errors.array() })
-  }
-
   const { email, password } = req.body
 
   const userExist = await User.findOne({ email })
@@ -32,4 +26,26 @@ export const createAccount = async (req: Request, res: Response) => {
 
   await user.save()
   res.status(201).send('Registro creado correctamente!!')
+}
+
+export const login = async (req: Request, res: Response) => {
+  const { email, password } = req.body
+
+  // Revisar si el usuario esta registrado
+  const user = await User.findOne({ email })
+
+  if (!user) {
+    const error = new Error('El usuario no existe!!')
+    return res.status(404).json({ error: error.message })
+  }
+
+  // Comprobar el password
+  const validPassword = await checkPassword(password, user.password)
+
+  if (!validPassword) {
+    const error = new Error('Password incorrecto!!')
+    return res.status(401).json({ error: error.message })
+  }
+
+  res.send('Autenticado!!')
 }
--- src/router.ts
+++ src/router.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express'
 import { body } from 'express-validator'
-import { createAccount } from './handlers'
+import { createAccount, login } from './handlers'
+import { handleInputErrors } from './middleware/validation'
 
 const router = Router()
 
@@ -11,7 +12,15 @@ router.post(
   body('name').notEmpty().withMessage('Nombre de usuario no puede ir vacio!!'),
   body('email').isEmail().withMessage('Email no valido!!'),
   body('password').isLength({ min: 6 }).withMessage('El password debe ser de 6 caracteres o mas'),
+  handleInputErrors,
   createAccount
 )
 
+router.post(
+  '/auth/login',
+  body('email').isEmail().withMessage('Email no valido!!'),
+  body('password').notEmpty().withMessage('El password es obligatorio!!'),
+  handleInputErrors,
+  login
+)
 export default router
